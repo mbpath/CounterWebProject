8df89b6b6b0012762ee1572b1962747d26e9bc7c
mbpath <34367811+mbpath@users.noreply.github.com>
2017-12-09 12:14:25 +0530

    Create CalcTest.java

---
+++ src/test/java/com/qaagility/controller/CalcTest.java
@@ -0,0 +1,22 @@
+package com.qaagility.controller;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalcTest{
+
+  public viod testSlow() {
+    System.out.println("slow");
+  }
+  public viod testSlower() {
+    System.out.println("slower");
+  }
+  public viod testFast() {
+    System.out.println("fast-updated");
+  }
+  
+ @Test  
+    public void testCalc() {
+      assertsquals("Result",9,9);
+}
+}
