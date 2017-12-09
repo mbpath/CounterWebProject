034eb9a1e9d9842ff3ede1a3d95d6a53f8de3104
mbpath <34367811+mbpath@users.noreply.github.com>
2017-12-09 12:52:38 +0530

    Update AppTest.java

--- src/test/java/com/qaagility/controller/AppTest.java
+++ src/test/java/com/qaagility/controller/AppTest.java
@@ -1,7 +1,7 @@
 package com.qaagility.controller;
 
 import org.junit.Test;
-import static org.juint.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 import com.qaagility.controller.Calculator;
 
 public class AppTest {
